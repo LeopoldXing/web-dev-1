394bc4da58d63bfa12bde1e365f0893888d24691
DiscoBroccoli <19515DearClem@gmail.com>
2024-03-02 16:01:41 -0600

    26 Feb, 2024, 上课内容

--- module6/script.js
+++ module6/script.js
@@ -1,5 +1,15 @@
-if (true) {
-  var tmp = 123;
-}
+const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
 
-console.log(tmp);
+console.log(days);
+console.log(days[0]);
+console.log(days[4]);
+
+let section = document.getElementsByTagName("section")[0];
+console.log(section);
+
+section.innerHTML += "hello";
+
+let div = document.getElementById("home");
+for (let i = 0; i < days.length; i++) {
+  div.innerHTML += `<p>${days[i]}</p>`;
+}
