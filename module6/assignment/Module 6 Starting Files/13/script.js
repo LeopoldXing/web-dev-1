c11d449eb3da82fbcb6aae47e7225f08a3383aae
lupingxing2 <lxing2@rrc.ca>
2024-03-08 21:49:04 -0600

    assignment 6, competency 13 完成

--- module6/assignment/Module 6 Starting Files/13/script.js
+++ module6/assignment/Module 6 Starting Files/13/script.js
@@ -1,25 +1,26 @@
 /********f************
 
-	Do not alter this comment block. 
+	Do not alter this comment block.
 	Only fill out the information below.
 
 	Competency 13 Event Listeners
-	Name: 
-	Date: 
-	Description: 
+	Name: Luping Xing
+	Date: Mar 8, 2024
+	Description: Assignment 6, Competency 13 - Event Listeners
 
 *********************/
 
 //	Task 1: Event listener to trigger the load function upon DOM loading
+document.addEventListener("DOMContentLoaded", load);
 
-
-/*	
+/*
 	Task 2
 	Load function
 	Event listeners to setup the page will go here
 */
 function load() {
-
+	let button = document.getElementsByTagName("button")[0];
+	button.addEventListener("click", clickMe);
 }
 
 /*
@@ -28,7 +29,5 @@ function load() {
 	This will change the display value of the input
 */
 function clickMe() {
-
+	document.getElementById("popup").style.display = "block";
 }
-
-
