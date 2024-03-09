2920bc2e77290e5dab0992a62d7776f3beb10f2f
lupingxing2 <lxing2@rrc.ca>
2024-03-08 22:02:22 -0600

    assignment 6, competency 14 完成

--- module6/assignment/Module 6 Starting Files/14/script.js
+++ module6/assignment/Module 6 Starting Files/14/script.js
@@ -1,23 +1,32 @@
 /********f************
 
-    Do not alter this comment block. 
+    Do not alter this comment block.
     Only fill out the information below.
 
-    Competency 14 
-    Name: 
-    Date:
-    Description:
+    Competency 14
+    Name: Luping Xing
+    Date: Mar 8, 2024
+    Description: Assignment 6, Competency 14 - Manipulating Elements
 
 *********************/
 
 document.addEventListener("DOMContentLoaded", load);
 
-/*	
+/*
     Load function
     Create, insert, and delete elements
 */
 function load() {
+  let loremString = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet autem, culpa dolorem dolorum enim exercitationem explicabo impedit magni maxime minima neque tempore vel voluptatibus. Architecto exercitationem explicabo laborum possimus voluptatem?";
+  let p = document.createElement("p");
+  p.innerHTML = loremString;
+  document.getElementById("results").appendChild(p);
 
+  let h2 = document.createElement("h2");
+  h2.innerHTML = "Some lorem texts";
+  p.parentNode.insertBefore(h2, p);
+
+  document.getElementsByTagName("footer")[0].remove();
 }
 
 
