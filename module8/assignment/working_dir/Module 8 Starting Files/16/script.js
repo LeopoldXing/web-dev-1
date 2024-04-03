6d2b380a2283b530dfab6d5770552a95e27f3766
DiscoBroccoli <19515DearClem@gmail.com>
2024-04-03 00:10:53 -0500

    competency16 重写

--- module8/assignment/working_dir/Module 8 Starting Files/16/script.js
+++ module8/assignment/working_dir/Module 8 Starting Files/16/script.js
@@ -1,14 +1,14 @@
 /********f************
 
-    Do not alter this comment block.
-    Only fill out the information below.
+ Do not alter this comment block.
+ Only fill out the information below.
 
-    Competency 16
-    Name: Luping Xing
-    Date: Apr 2, 2024
-    Description: script file of Module 8 competency 16
+ Competency 16
+ Name: Luping Xing
+ Date: Apr 2, 2024
+ Description: script file of Module 8 competency 16
 
-*********************/
+ *********************/
 
 
 /*
@@ -18,13 +18,13 @@
  */
 function load() {
 
-    fetch("https://dog.ceo/api/breeds/list/all")
-        .then(result => {
-            return result.json();
-        })
-        .then(data => {
-            createHTML(data);
-        });
+  fetch("https://dog.ceo/api/breeds/list/all")
+    .then(result => {
+      return result.json();
+    })
+    .then(data => {
+      createHTML(data);
+    });
 
 }
 
@@ -37,18 +37,32 @@ function createHTML(data) {
   let mainElement = document.getElementById("wrapper");
 
   let div = document.createElement("div");
-  let ol = document.createElement("ol");
-  let bullDogLabel = document.createElement("span");
-  bullDogLabel.innerText = "Bull Dog: ";
-  let bullDogList = data.message.bulldog;
-  for(let i = 0; i < bullDogList.length; i++) {
-    let li = document.createElement("li");
-    li.innerText = bullDogList[i];
-    ol.appendChild(li);
-  }
 
-  div.appendChild(bullDogLabel);
-  div.appendChild(ol);
+  let message = data.message;
+  let keys = Object.keys(message);
+  let values = Object.values(message);
+
+  for (let i = 0; i < keys.length; i++) {
+    let currentBreed = values[i];
+
+    if (currentBreed && currentBreed.length > 0) {
+      let ol = document.createElement("ol");
+      let label = document.createElement("span");
+      label.innerText = keys[i] + ": ";
+
+      for (let j = 0; j < currentBreed.length; j++) {
+        let li = document.createElement("li");
+        li.innerText = currentBreed[j];
+        ol.appendChild(li);
+      }
+
+      div.appendChild(label);
+      div.appendChild(ol);
+      if (i !== keys.length - 1) {
+        div.appendChild(document.createElement("hr"));
+      }
+    }
+  }
 
   mainElement.appendChild(div);
 }
