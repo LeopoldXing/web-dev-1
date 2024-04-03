bb971f7806bb368d1f205feebb509a4fed827286
DiscoBroccoli <19515DearClem@gmail.com>
2024-04-03 14:52:40 -0500

    apr 3, 2024 programming 练习

---
+++ module6/programming/programming1.js
@@ -0,0 +1,19 @@
+/*function clickThisFunc(name) {
+  console.log(name);
+}
+
+let radioElement = document.getElementById("radio1");
+radioElement.addEventListener("click", function () {
+  clickThisFunc("rose")
+})*/
+
+/*let phoneElement = document.getElementById("phone");
+
+function is15Digits() {
+  return phoneElement.value.length === 15;
+}
+
+phoneElement.addEventListener("change", () => console.log(is15Digits()));*/
+
+/*let creditCard = document.getElementsByTagName("input")[2];
+creditCard.focus();*/
